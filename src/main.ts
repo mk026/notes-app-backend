26bd4c05809d4ce47f354b92c2182df072254a2b
Mikhail <mk0602604@gmail.com>
2022-03-28 14:34:03 +0300

    Use note router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import config from './config/config';
+import noteRouter from './routes/Note';
 
 const app = express();
+
+app.use('/api', noteRouter);
 
 app.listen(config.server.port, () => {
   console.log(`Server listening on port ${config.server.port}`);
--- src/routes/Note.ts
+++ src/routes/Note.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => res.json({ message: 'GET Notes' }));
-router.post('/', (req, res) => res.json({ message: 'POST Notes' }));
-router.put('/', (req, res) => res.json({ message: 'PUT Notes' }));
-router.delete('/', (req, res) => res.json({ message: 'DELETE Notes' }));
+router.get('/notes', (req, res) => res.json({ message: 'GET Notes' }));
+router.post('/notes', (req, res) => res.json({ message: 'POST Notes' }));
+router.put('/notes', (req, res) => res.json({ message: 'PUT Notes' }));
+router.delete('/notes', (req, res) => res.json({ message: 'DELETE Notes' }));
 
 export default router;
