2d2b9228e3823534f0089dfdb393fab000d06257
Mikhail <mk0602604@gmail.com>
2022-03-29 13:06:53 +0300

    Use todo router

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import config from './config/config';
 import noteRouter from './routes/Note';
+import todoRouter from './routes/Todo';
 
 const app = express();
 
 app.use('/api', noteRouter);
+app.use('/api', todoRouter);
 
 app.listen(config.server.port, () => {
   console.log(`Server listening on port ${config.server.port}`);
