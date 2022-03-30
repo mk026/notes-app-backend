a0fc08709faf56d988dde0f96b026a42c787fd4e
Mikhail <mk0602604@gmail.com>
2022-03-30 13:36:01 +0300

    Use cors middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 import config from './config/config';
 import noteRouter from './routes/Note';
 import todoRouter from './routes/Todo';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', noteRouter);
