c94a354aedf1896880c87daa7bc5a8cedd1de1a5
Mikhail <mk0602604@gmail.com>
2022-04-02 13:45:21 +0300

    Use interfaces in note/todo models

--- src/api/note/note.model.ts
+++ src/api/note/note.model.ts
@@ -1,8 +1,10 @@
 import { Schema, model } from 'mongoose';
 
+import INote from './note.interface';
+
 const noteSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
 });
 
-export default model('Note', noteSchema);
+export default model<INote>('Note', noteSchema);
--- src/api/todo/todo.model.ts
+++ src/api/todo/todo.model.ts
@@ -1,8 +1,10 @@
 import { Schema, model } from 'mongoose';
 
+import ITodo from './todo.interface';
+
 const todoSchema = new Schema({
   title: { type: String, required: true },
   completed: { type: Boolean, required: true },
 });
 
-export default model('Todo', todoSchema);
+export default model<ITodo>('Todo', todoSchema);
