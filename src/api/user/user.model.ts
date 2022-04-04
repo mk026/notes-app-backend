9f5dafd125563f80da07a9dad41a913ebc712426
Mikhail <mk0602604@gmail.com>
2022-04-04 20:39:32 +0300

    Create user interface

--- src/api/user/user.model.ts
+++ src/api/user/user.model.ts
@@ -1,9 +1,11 @@
 import { Schema, model } from 'mongoose';
 
+import IUser from './user.interface';
+
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-export default model('User', userSchema);
+export default model<IUser>('User', userSchema);
