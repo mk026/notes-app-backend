b15c1e6612cadf349c5837b5243f9e07e423234f
Mikhail <mk0602604@gmail.com>
2022-04-05 15:13:59 +0300

    Create user service

---
+++ src/api/user/user.service.ts
@@ -0,0 +1,15 @@
+class UserService {
+  async getOne() {
+    return null;
+  }
+
+  async update() {
+    return null;
+  }
+
+  async delete() {
+    return null;
+  }
+}
+
+export default new UserService();
