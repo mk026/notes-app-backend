fea677f562da4585640fea0a9056f6762a02e194
Mikhail <mk0602604@gmail.com>
2022-04-05 15:41:57 +0300

    Catch errors inside of user controller

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -4,18 +4,30 @@ import UserService from './user.service';
 
 class UserController {
   async getOne(req: Request, res: Response) {
-    const user = await UserService.getOne(req.params.id);
-    return res.json(user);
+    try {
+      const user = await UserService.getOne(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 
   async update(req: Request, res: Response) {
-    const user = await UserService.update(req.body);
-    return res.json(user);
+    try {
+      const user = await UserService.update(req.body);
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 
   async delete(req: Request, res: Response) {
-    const user = await UserService.delete(req.params.id);
-    return res.json(user);
+    try {
+      const user = await UserService.delete(req.params.id);
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 }
 
