03cf038263039c50cf5a6473a00b6c9c7470dd2c
Mikhail <mk0602604@gmail.com>
2022-04-06 16:00:28 +0300

    Use auth service in auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 
+import AuthService from './auth.service';
+
 class AuthController {
   async signup(req: Request, res: Response) {
-    return res.json({ message: 'Signup' });
+    const msg = await AuthService.signup();
+    return res.json(msg);
   }
 
   async signin(req: Request, res: Response) {
-    return res.json({ message: 'Signin' });
+    const msg = await AuthService.signin();
+    return res.json(msg);
   }
 }
 
