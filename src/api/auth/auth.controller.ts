b486fddf5c5207f4e2afea3cec239dfb7efd86c2
Mikhail <mk0602604@gmail.com>
2022-04-07 15:39:14 +0300

    Catch errors inside of auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -4,13 +4,21 @@ import AuthService from './auth.service';
 
 class AuthController {
   async signup(req: Request, res: Response) {
-    const msg = await AuthService.signup();
-    return res.json(msg);
+    try {
+      const msg = await AuthService.signup();
+      return res.json(msg);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 
   async signin(req: Request, res: Response) {
-    const msg = await AuthService.signin();
-    return res.json(msg);
+    try {
+      const msg = await AuthService.signin();
+      return res.json(msg);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 }
 
