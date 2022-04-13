a69c656bc3705295bd3c33a24feb6cb2a66ef379
Mikhail <mk0602604@gmail.com>
2022-04-13 17:31:27 +0300

    Check if email is already in use

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -5,9 +5,13 @@ import User from '../user/user.model';
 class AuthService {
   async signup(user: { name: string; email: string; password: string }) {
     const { name, email, password } = user;
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      return { message: `User with email ${email} already exists` };
+    }
     const hashPassword = bcrypt.hashSync(password);
     await User.create({ name, email, password: hashPassword });
-    return { message: `Successfuly signed up ${user.name}` };
+    return { message: `Successfuly signed up ${name}` };
   }
 
   async signin(user: { email: string; password: string }) {
