904c36365e808342ac0554b75aa067f749447e9c
Mikhail <mk0602604@gmail.com>
2022-04-13 14:04:19 +0300

    Save new users in db with hashed passwords

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -1,6 +1,13 @@
+import bcrypt from 'bcryptjs';
+
+import User from '../user/user.model';
+
 class AuthService {
   async signup(user: { name: string; email: string; password: string }) {
-    return { message: `Signup ${user.name}` };
+    const { name, email, password } = user;
+    const hashPassword = bcrypt.hashSync(password);
+    await User.create({ name, email, password: hashPassword });
+    return { message: `Successfuly signed up ${user.name}` };
   }
 
   async signin(user: { email: string; password: string }) {
