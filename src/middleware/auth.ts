1c5a380eb9d468019dcc29022262bafe78506e58
Mikhail <mk0602604@gmail.com>
2022-04-18 16:09:45 +0300

    Create auth middleware to extract jwt

---
+++ src/middleware/auth.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+import config from '../config/config';
+
+export default function authMiddleware(
+  req: Request & { user: string | JwtPayload },
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+    const secret = config.server.token.secret;
+    const decodedData = jwt.verify(token, secret);
+    req.user = decodedData;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Unauthorized' });
+  }
+}
