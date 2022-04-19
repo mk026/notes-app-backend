744c791268634a691d562813119af7115c73dba4
Mikhail <mk0602604@gmail.com>
2022-04-19 22:37:41 +0300

    Protect note/todo routes with auth middleware

--- src/routes/note/note.router.ts
+++ src/routes/note/note.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+
 import NoteController from './note.controller';
+import authMiddleware from '../../middleware/auth';
 
 const router = Router();
 
-router.get('/notes', NoteController.getAll);
-router.post('/notes', NoteController.create);
-router.put('/notes', NoteController.update);
-router.delete('/notes/:id', NoteController.delete);
+router.get('/notes', authMiddleware, NoteController.getAll);
+router.post('/notes', authMiddleware, NoteController.create);
+router.put('/notes', authMiddleware, NoteController.update);
+router.delete('/notes/:id', authMiddleware, NoteController.delete);
 
 export default router;
--- src/routes/todo/todo.router.ts
+++ src/routes/todo/todo.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
+
 import TodoController from './todo.controller';
+import authMiddleware from '../../middleware/auth';
 
 const router = Router();
 
-router.get('/todos', TodoController.getAll);
-router.post('/todos', TodoController.create);
-router.put('/todos', TodoController.update);
-router.delete('/todos/:id', TodoController.delete);
+router.get('/todos', authMiddleware, TodoController.getAll);
+router.post('/todos', authMiddleware, TodoController.create);
+router.put('/todos', authMiddleware, TodoController.update);
+router.delete('/todos/:id', authMiddleware, TodoController.delete);
 
 export default router;
