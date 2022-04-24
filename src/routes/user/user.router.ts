aac50eeecdd9903160ba6b16fd310706d0c80312
Mikhail <mk0602604@gmail.com>
2022-04-24 22:30:51 +0300

    Protect user routes with auth middleware

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './user.controller';
+import authMiddleware from '../../middleware/auth';
 
 const router = Router();
 
-router.get('/users', UserController.getOne);
-router.put('/users', UserController.update);
-router.delete('/users:id', UserController.delete);
+router.get('/users', authMiddleware, UserController.getOne);
+router.put('/users', authMiddleware, UserController.update);
+router.delete('/users:id', authMiddleware, UserController.delete);
 
 export default router;
