adcc6a545b1c5d741cff9ee64eb50c0c978c60a0
Mikhail <mk0602604@gmail.com>
2022-05-22 16:03:16 +0300

    Refactor error handling in note/todo controllers

--- src/routes/note/note.controller.ts
+++ src/routes/note/note.controller.ts
@@ -1,45 +1,56 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import IUser from '../user/user.interface';
 import INote from './note.interface';
 import noteService from './note.service';
 
 class NoteController {
-  async getAll(req: Request & { user?: { id: IUser['_id'] } }, res: Response) {
+  async getAll(
+    req: Request & { user?: { id: IUser['_id'] } },
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const notes = await noteService.getAll(req.user!.id);
       return res.json(notes);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async create(req: Request & { user?: { id: IUser['_id'] } }, res: Response) {
+  async create(
+    req: Request & { user?: { id: IUser['_id'] } },
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const noteData = { ...req.body, userId: req.user?.id };
       const note = await noteService.create(noteData);
       return res.json(note);
     } catch (error) {
-      console.log(error);
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async update(req: Request, res: Response) {
+  async update(req: Request, res: Response, next: NextFunction) {
     try {
       const note = await noteService.update(req.body);
       return res.json(note);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async delete(req: Request<{ id: INote['_id'] }>, res: Response) {
+  async delete(
+    req: Request<{ id: INote['_id'] }>,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const note = await noteService.delete(req.params.id);
       return res.json(note);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 }
--- src/routes/todo/todo.controller.ts
+++ src/routes/todo/todo.controller.ts
@@ -1,44 +1,56 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import IUser from '../user/user.interface';
 import ITodo from './todo.interface';
 import todoService from './todo.service';
 
 class TodoController {
-  async getAll(req: Request & { user?: { id: IUser['_id'] } }, res: Response) {
+  async getAll(
+    req: Request & { user?: { id: IUser['_id'] } },
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const todos = await todoService.getAll(req.user!.id);
       return res.json(todos);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async create(req: Request & { user?: { id: IUser['_id'] } }, res: Response) {
+  async create(
+    req: Request & { user?: { id: IUser['_id'] } },
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const todoData = { ...req.body, userId: req.user?.id };
       const todo = await todoService.create(todoData);
       return res.json(todo);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async update(req: Request, res: Response) {
+  async update(req: Request, res: Response, next: NextFunction) {
     try {
       const todo = await todoService.update(req.body);
       return res.json(todo);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 
-  async delete(req: Request<{ id: ITodo['_id'] }>, res: Response) {
+  async delete(
+    req: Request<{ id: ITodo['_id'] }>,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const todo = await todoService.delete(req.params.id);
       return res.json(todo);
     } catch (error) {
-      return res.status(500).json(error);
+      next(error);
     }
   }
 }
