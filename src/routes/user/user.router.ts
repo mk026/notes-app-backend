49883e8dc9775b6a95c9202e39549b6abe54b814
Mikhail <mk0602604@gmail.com>
2022-06-03 23:43:26 +0300

    Fix path for delete user route

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -6,6 +6,6 @@ import authMiddleware from '../../middleware/auth.middleware';
 const router = Router();
 
 router.put('/users', authMiddleware, userController.update);
-router.delete('/users:id', authMiddleware, userController.delete);
+router.delete('/users/:id', authMiddleware, userController.delete);
 
 export default router;
