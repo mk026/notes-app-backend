4903c8865a8f515af17e50c328946543fead468e
Mikhail <mk0602604@gmail.com>
2022-06-30 23:43:34 +0300

    Add validation rules to config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,9 +3,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SERVER_PORT = +process.env.SERVER_PORT!;
+
 const MONGODB_HOST = process.env.MONGODB_HOST!;
+
 const TOKEN_SECRET = process.env.TOKEN_SECRET!;
 const TOKEN_EXPIRESIN = process.env.TOKEN_EXPIRESIN!;
+
+const TODO_TITLE_MIN_LENGTH = 1;
+const TODO_TITLE_MAX_LENGTH = 100;
+
+const NOTE_TITLE_MIN_LENGTH = 1;
+const NOTE_TITLE_MAX_LENGTH = 100;
+const NOTE_CONTENT_MIN_LENGTH = 1;
+const NOTE_CONTENT_MAX_LENGTH = 1000;
+
+const USER_NAME_MIN_LENGTH = 1;
+const USER_NAME_MAX_LENGTH = 100;
+const USER_PASSWORD_MIN_LENGTH = 6;
+const USER_PASSWORD_MAX_LENGTH = 100;
 
 const config = {
   server: {
@@ -13,6 +28,32 @@ const config = {
     token: {
       secret: TOKEN_SECRET,
       expiresIn: TOKEN_EXPIRESIN,
+    },
+    validation: {
+      todo: {
+        minLength: TODO_TITLE_MIN_LENGTH,
+        maxLength: TODO_TITLE_MAX_LENGTH,
+      },
+      note: {
+        title: {
+          minLength: NOTE_TITLE_MIN_LENGTH,
+          maxLength: NOTE_TITLE_MAX_LENGTH,
+        },
+        content: {
+          minLength: NOTE_CONTENT_MIN_LENGTH,
+          maxLength: NOTE_CONTENT_MAX_LENGTH,
+        },
+      },
+      user: {
+        name: {
+          minLength: USER_NAME_MIN_LENGTH,
+          maxLength: USER_NAME_MAX_LENGTH,
+        },
+        password: {
+          minLength: USER_PASSWORD_MIN_LENGTH,
+          maxLength: USER_PASSWORD_MAX_LENGTH,
+        },
+      },
     },
   },
   mongodb: {
