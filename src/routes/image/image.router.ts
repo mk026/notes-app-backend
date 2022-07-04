130ed95d0b90811b0801a8f59309a0409577646b
Mikhail <mk0602604@gmail.com>
2022-07-04 23:35:31 +0300

    Apply image upload middleware

--- src/routes/image/image.router.ts
+++ src/routes/image/image.router.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 
 import imageController from './image.controller';
 import authMiddleware from '../../middleware/auth.middleware';
+import upload from '../../middleware/upload.middleware';
 
 const router = Router();
 
 router.get('/images', authMiddleware, imageController.getAll);
 
-router.post('/images', authMiddleware, imageController.create);
+router.post(
+  '/images',
+  authMiddleware,
+  upload.single('image'),
+  imageController.create
+);
 
 router.put('/images', authMiddleware, imageController.update);
 
