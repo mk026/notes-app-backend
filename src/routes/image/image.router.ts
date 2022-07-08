539b8c796db31587f33035de25f0a4f39d82ab28
Mikhail <mk0602604@gmail.com>
2022-07-08 23:36:22 +0300

    Apply image validators and validation middleware

--- src/routes/image/image.router.ts
+++ src/routes/image/image.router.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import imageController from './image.controller';
 import authMiddleware from '../../middleware/auth.middleware';
 import upload from '../../middleware/upload.middleware';
+import { createImageValidator, updateImageValidator } from './image.validation';
+import validationMiddleware from '../../middleware/validation.middleware';
 
 const router = Router();
 
@@ -10,12 +12,20 @@ router.get('/images', authMiddleware, imageController.getAll);
 
 router.post(
   '/images',
+  createImageValidator,
+  validationMiddleware,
   authMiddleware,
   upload.single('image'),
   imageController.create
 );
 
-router.put('/images', authMiddleware, imageController.update);
+router.put(
+  '/images',
+  updateImageValidator,
+  validationMiddleware,
+  authMiddleware,
+  imageController.update
+);
 
 router.delete('/images/:id', authMiddleware, imageController.delete);
 
